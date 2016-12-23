f1a92736667f944ac68808488754481ea8f1faf4
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2016-12-23 13:29:20 +0300

    Initial Commit

---
+++ MetalBitonicSort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MetalBitonicSort
+//
+//  Created by Alexander Tarasikov on 23/12/2016.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
